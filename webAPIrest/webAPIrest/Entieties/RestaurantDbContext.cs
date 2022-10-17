1f5591ba20bf48e78f66bc6aa19d010a8126be51
Your Name <sansong@o2.pl>
2022-10-17 19:06:50 +0200

    2022 10 17

---
+++ webAPIrest/webAPIrest/Entieties/RestaurantDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webAPIrest.Entieties
+
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionSQL = "Server=BARTEK\\SQLEXPERS;Database=RestaurantDb;Trusted_Connection=True;";
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        
+        // wymagania do tabel bazy danych
+        //property 
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Address>()
+                .Property(a => a.Street)
+                .IsRequired();
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+
+             
+
+                
+            
+
+                
+        }
+        // konfiguracja bazy danych i połączenie
+        //configurate and conectionn data base
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionSQL);
+        }
+
+        //migracja w NuGet add-migration nazwa
+        //tworzenie bazy update-database
+    }
+}
---
+++ webAPIrest/webAPIrest/RestaurantSeeder.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml.Serialization;
+using webAPIrest.Entieties;
+
+namespace webAPIrest
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }  
+        public void Seed()
+        {
+            if(_dbContext.Database.CanConnect())
+            {
+                if(!_dbContext.Restaurants.Any())
+                {
+                    var restauants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restauants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+        public IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restauants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name ="MCDonald",
+                    Category="Fast Food",
+                    Description="Buergers and chips",
+                    ContactNumber="798625654",
+                    Email= "bartoszkwiatkowski1996@gmail.com",
+                    Delivery=true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name="Mcroyal",
+                            Price=10.20M,
+                            Description=" kanapka premium"
+
+                        },
+                         new Dish()
+                        {
+                             Name="Chips Small",
+                             Price=5M,
+                             Description=" Frytki"
+
+
+
+                        },
+                          new Dish()
+                        {
+                              Name="MC Burger",
+                              Price=10M,
+                              Description=" mack burger"
+
+                        }
+                    },
+
+                    Address = new Address()
+                    {
+                        City="Gdańsk",
+                        Street="Wałowa",
+                        PostalCode="80-858"
+
+
+                    }
+
+
+
+
+                }
+
+            };
+            return restauants;
+        }
+    }
+}
